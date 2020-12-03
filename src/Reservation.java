30add3e1b25822db44ab74b6f71f0dec3897aa66
Ibrahim Marou <ib200034@gmail.com>
2020-12-02 18:32:24 -0600

    just pseudocode for reservations

---
+++ src/Reservation.java
@@ -0,0 +1,19 @@
+/*import java.sql.*;
+
+public class Reservation{
+  
+ private int resID;
+ 
+ private String category;
+  
+ private Reservation(int _resID, String _category){
+  setResID(_resID);
+  setcategory(_category);
+ }
+ public Reservation(String resID, int start, int end, String category){
+    
+  
+  
+  
+  
+}*/
